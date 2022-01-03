f1b20f9ffce2ead6cef68f7c9c53284516b29c1d
Gokul <gokulchandan18@gmail.com>
2022-01-03 22:11:49 +0530

    commit

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnChanges {
+
+
+  @Input()
+  loggedIn!: boolean;
+  message!: string;
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges){
+    console.log(changes);
+      
   }
 
 }
