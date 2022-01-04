9cea55c6a6cd1a4ecd00be7624ecf169eda17afe
Gokul <gokulchandan18@gmail.com>
2022-01-04 08:47:18 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  [x: string]: any;
+export class AppComponent implements AfterViewInit {
+  [x: string]: any; 
   userLoggedIn = false;
+
+  @ViewChild(ChildComponent)
+  childComponentRef!: ChildComponent;
+
+  ngAfterViewInit(){
+this.childComponentRef.message='Message from parent component';
+  }
 }
